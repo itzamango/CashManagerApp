85a2818daf5236910e86c5137aae33beec21f590
Itzam <fernandonovca@gmail.com>
2023-05-18 16:25:52 -0600

    Initial commit

---
+++ app/src/main/java/org/itza/cashmanager/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package org.itza.cashmanager.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
